6d89358aade8551f3efa8c84ca06b8a1ef5fe75c
jshyun91Cloud <jshyun91@gmail.com>
2020-04-16 19:41:56 +0200

    designed ViewGoods

--- frontend/src/pages/ViewGoods.js
+++ frontend/src/pages/ViewGoods.js
@@ -1,11 +1,34 @@
 import React from "react";
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 const API_URL = 'http://localhost:5000'
 
+const useStyles = makeStyles({
+  root: {
+  //  maxWidth: 345,
+    maxWidth: 600,
+  },
+  media: {
+//    height: 140,
+    height: 400,
+  },
+});
+
+
 export default function ViewGoods() {
   const [products, setProducts] = useState([]);
+  const classes = useStyles();
+
 
   function loadContents() {
     console.log('loadContents');
@@ -28,21 +51,53 @@ export default function ViewGoods() {
   }, []);
 
   return (
-    <div >
+    <div align="center" >
       <React.Fragment>
+        <div><p></p></div>
         {products.map((card) => (
           <div key={card._id}>
-            <div>_id: {card._id}</div>
+            {/* <div>_id: {card._id}</div>
             <div>title: {card.title}</div>
             <div>userId: {card.userId}</div>
             <div>price: {card.price}</div>
             <div>description: {card.description}</div>
             <div>image: {card.image}</div>
-            <div>createdAt: {card.createdAt}</div>
+            <div>createdAt: {card.createdAt}</div> */}
+
             <div>
-              <p></p>
-            </div>  
-          </div>        
+              <Card className={classes.root}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={API_URL + '/imageFiles/' + card.image}
+                    title={card.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </div>
+            <div><p></p></div>
+
+            <div align="center">
+              <Card className={classes.root}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {card.price} 원
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {card.description}
+                      <p></p>등록일: {card.createdAt}
+                    </Typography>
+
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+
+            </div>
+          </div>
         ))}
       </React.Fragment>
     </div>
