1a4ccd72ebc6a7e0e57204de4aff4726cdc81cc2
jshyun91Cloud <jshyun91@gmail.com>
2020-04-22 15:19:03 +0200

    Add ButtonAppBar func. Add SignUp page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,24 @@ import About from './pages/About';
 import RegisterGoods from './pages/RegisterGoods';
 import ListGoods from './pages/ListGoods';
 import ViewGoods from './pages/ViewGoods';
+import SignUp from './pages/SignUp';
 
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <AppBar position="relative">
+        
+        <ButtonAppBar/>
+        {/* <AppBar position="relative">
           <Toolbar>            
             <Typography variant="h6" color="inherit" noWrap>
             <Link style={{ textDecoration: 'none' }} to='/'>
@@ -26,16 +33,64 @@ class App extends Component {
               <Button variant="outlined" color="primary">상품 등록</Button>
             </Link>
           </Toolbar>
-        </AppBar>
+        </AppBar> */}
         
         <Route exact path='/' component={ListGoods} />
         <Route path='/about' component={About} />
         <Route path='/listgoods' component={ListGoods} />
         <Route path='/registergoods' component={RegisterGoods} />        
         <Route path='/viewgoods' component={ViewGoods} />        
+        <Route path='/signup' component={SignUp} />        
       </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(30),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="primary" aria-label="menu">
+            <Link to='/' color="inherit" >
+              <MenuIcon />
+            </Link>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            OE & FA Market
+          </Typography>
+          <Link to='/'>
+            <Button color="primary" variant="contained">home</Button>
+          </Link>
+          <Link to='/registergoods'>
+            <Button color="primary" variant="contained">상품 등록</Button>
+          </Link>
+          <Link to='/Login'>
+            <Button color="primary" variant="contained">Login</Button>
+          </Link>
+          <Link to='/SignUp'>
+            <Button color="primary" variant="contained">Sign in</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as About } from './About';
 export { default as ListGoods } from './ListGoods';
 export { default as RegisterGoods } from './RegisterGoods';
 export { default as ViewGoods } from './ViewGoods';
+export { default as SignUp } from './SignUp';
